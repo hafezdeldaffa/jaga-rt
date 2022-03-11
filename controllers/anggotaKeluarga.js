7bd406f240790143c01446b03453c2f437cb6ffe
Hafezd El Daffa <hafezdeldaffa9@gmail.com>
2022-03-11 23:03:36 +0700

    create edit & delete anggotaKeluarga functionality

--- controllers/anggotaKeluarga.js
+++ controllers/anggotaKeluarga.js
@@ -1,15 +1,15 @@
-const AnggotaKeluarga = require("../models/anggotaKeluarga");
-const Keluarga = require("../models/keluarga");
-const { errorHandling } = require("./errorHandling");
-const { validationResult } = require("express-validator");
-const RT = require("../models/rt");
+const AnggotaKeluarga = require('../models/anggotaKeluarga');
+const Keluarga = require('../models/keluarga');
+const { errorHandling } = require('./errorHandling');
+const { validationResult } = require('express-validator');
+const RT = require('../models/rt');
 
 exports.addAnggotaKeluarga = async (req, res, next) => {
   try {
     /* Creating validation */
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      const error = new Error("Validation error, entered data is incorrect");
+      const error = new Error('Validation error, entered data is incorrect');
       error.statusCode = 422;
       throw err;
     }
@@ -17,7 +17,7 @@ exports.addAnggotaKeluarga = async (req, res, next) => {
     /* Get data from jwt */
     const { email, role } = req.user;
 
-    if (role === "Keluarga") {
+    if (role === 'Keluarga') {
       const { nama, role, statusCovid } = req.body;
 
       const keluarga = await Keluarga.findOne({ email: email });
@@ -34,7 +34,7 @@ exports.addAnggotaKeluarga = async (req, res, next) => {
 
       // const keluargaUpdated = await Keluarga.findByIdAndUpdate(keluargaId, newKeluarga);
 
-      res.json({ message: "Berhasil menambahkan anggota keluarga", anggota });
+      res.json({ message: 'Berhasil menambahkan anggota keluarga', anggota });
     }
   } catch (error) {
     /* Handling Errors */
@@ -48,7 +48,7 @@ exports.getAnggotaKeluarga = async (req, res, next) => {
     /* Creating validation */
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      const error = new Error("Validation error, entered data is incorrect");
+      const error = new Error('Validation error, entered data is incorrect');
       error.statusCode = 422;
       throw err;
     }
@@ -59,13 +59,105 @@ exports.getAnggotaKeluarga = async (req, res, next) => {
     if (role === 'Keluarga') {
       const keluarga = await Keluarga.findOne({ email: email });
       const keluargaId = keluarga._id;
-  
+
       const anggotaKeluarga = await AnggotaKeluarga.find({
         keluargaId: keluargaId,
-      }).populate('keluargaId', 'namaKepalaKeluarga nomorRumah',);
-  
-      res.json({ message: "Anggota keluarga found", anggotaKeluarga });
-    } 
+      }).populate('keluargaId', 'namaKepalaKeluarga nomorRumah rt');
+
+      res.json({ message: 'Anggota keluarga found', anggotaKeluarga });
+    }
+  } catch (error) {
+    /* Handling Errors */
+    errorHandling(error);
+    next(error);
+  }
+};
+
+exports.getAnggotaKeluargaById = async (req, res, next) => {
+  try {
+    /* Creating validation */
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new Error('Validation error, entered data is incorrect');
+      error.statusCode = 422;
+      throw err;
+    }
+
+    const { id } = req.params;
+
+    /* Get data from jwt */
+    const { email, role } = req.user;
+
+    if (role === 'Keluarga') {
+      const anggota = await AnggotaKeluarga.findById(id).populate(
+        'keluargaId',
+        'namaKepalaKeluarga nomorRumah rt'
+      );
+
+      res.json({ message: 'Anggota by id found', anggota });
+    }
+  } catch (error) {
+    /* Handling Errors */
+    errorHandling(error);
+    next(error);
+  }
+};
+
+exports.editAnggotaKeluarga = async (req, res, next) => {
+  try {
+    /* Creating validation */
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new Error('Validation error, entered data is incorrect');
+      error.statusCode = 422;
+      throw err;
+    }
+
+    const { id } = req.params;
+
+    /* Get data from jwt */
+    const { email, role } = req.user;
+
+    if (role === 'Keluarga') {
+      const { nama, statusCovid, role } = req.body;
+
+      const newAnggota = {
+        nama: nama,
+        role: role,
+        statusCovid: statusCovid,
+      };
+
+      await AnggotaKeluarga.findOneAndUpdate({ _id: id }, newAnggota);
+
+      res.json({ message: 'Update anggota success' });
+    }
+  } catch (error) {
+    /* Handling Errors */
+    errorHandling(error);
+    next(error);
+  }
+};
+
+exports.deleteAnggotaKeluarga = async (req, res, next) => {
+  try {
+    /* Creating validation */
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new Error('Validation error, entered data is incorrect');
+      error.statusCode = 422;
+      throw err;
+    }
+
+    const { id } = req.params;
+
+    /* Get data from jwt */
+    const { email, role } = req.user;
+
+    if (role === 'Keluarga') {
+      await AnggotaKeluarga.findByIdAndDelete(id);
+
+      res.json({ message: 'Delete anggota keluarga success' });
+    }
   } catch (error) {
     /* Handling Errors */
     errorHandling(error);
--- routes/anggotaKeluarga.js
+++ routes/anggotaKeluarga.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const { addAnggotaKeluarga, getAnggotaKeluarga } = require('../controllers/anggotaKeluarga');
+const {
+  addAnggotaKeluarga,
+  getAnggotaKeluarga,
+  getAnggotaKeluargaById,
+  editAnggotaKeluarga,
+  deleteAnggotaKeluarga,
+} = require('../controllers/anggotaKeluarga');
 const { authenticateJWT } = require('../controllers/auth');
 const router = express.Router();
 
 router.get('/anggotaKeluarga', authenticateJWT, getAnggotaKeluarga);
-router.get('/anggotaKeluarga/:id');
+router.get('/anggotaKeluarga/:id', authenticateJWT, getAnggotaKeluargaById);
 router.post('/anggotaKeluarga', authenticateJWT, addAnggotaKeluarga);
-router.put('/anggotaKeluarga/:id');
-router.delete('/anggotaKeluarga/:id');
+router.put('/anggotaKeluarga/:id', authenticateJWT, editAnggotaKeluarga);
+router.delete('/anggotaKeluarga/:id', authenticateJWT, deleteAnggotaKeluarga);
 
 module.exports = router;
